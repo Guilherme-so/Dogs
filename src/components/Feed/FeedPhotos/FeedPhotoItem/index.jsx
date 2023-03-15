c41d0b8f2cb697fe24b3a5ac9aaa98df2e39c246
Guilherme <guilhermemagno09@gmail.com>
2023-03-15 19:12:15 -0300

    adding Image skeleton component

--- src/components/Feed/FeedPhotos/FeedPhotoItem/index.jsx
+++ src/components/Feed/FeedPhotos/FeedPhotoItem/index.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setModalOpen } from "../../../../Redux/user/userSlice";
+import Image from "../../../Helpers/Image/index";
 import styles from "./feedPhotoItem.module.css";
 
 function FeedPhotoItem({ foto }) {
   const dispatch = useDispatch();
 
   return (
-    <li className={styles.photo} onClick={()=> dispatch(setModalOpen(foto))}>
-      <img src={foto.src} alt={foto.title} />
+    <li className={styles.photo} onClick={() => dispatch(setModalOpen(foto))}>
+      <Image src={foto.src} alt={foto.title} />
       <span className={styles.visualisacao}>{foto.acessos}</span>
     </li>
   );
