9cee4f6fa03711bab57b549394028ee465ff1521
Guilherme <guilhermemagno09@gmail.com>
2023-03-16 18:26:05 -0300

    password lost page

--- src/components/Auth/PasswordLost/index.jsx
+++ src/components/Auth/PasswordLost/index.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import useForm from "../../../Hooks/useForm";
+import Input from "../../Forms/Input";
+import Button from "../../Forms/Button/Index";
+import styles from "./passwordLost.module.css";
 
 function LoginPasswordLost() {
+  const email = useForm();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div>LoginPasswordLost</div>
-  )
+    <section className={`${styles.passwordLost} animeLeft`}>
+      <form onSubmit={handleSubmit} className={styles.forms}>
+        <h1 className="title">Perdeu a senha?</h1>
+        <Input label="Email / Usuario" type="text" name="login" {...email} />
+        <Button>Enviar Email</Button>
+
+        {/* {loading ? (
+          <Button disabled>Enviando...</Button>
+        ) : (
+          <Button>Enviar Email</Button>
+        )} */}
+      </form>
+    </section>
+  );
 }
 
-export default LoginPasswordLost
+export default LoginPasswordLost;
